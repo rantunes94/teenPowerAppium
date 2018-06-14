e2361fabc3e48d0bdbb71a2c81401f4010fb0f98
Tunes94 <bryzon94@gmail.com>
2018-06-14 13:03:02 +0100

    ola
    Signed-off-by: Tunes94 <bryzon94@gmail.com>

---
+++ src/test/java/scenarios/TestCasePerfil1.java
@@ -0,0 +1,71 @@
+package scenarios;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestCasePerfil1 {
+    protected AndroidDriver driver;
+
+    @BeforeTest
+    protected void prepareAndroidForAppium() throws MalformedURLException {
+        File appDir = new File("C:\\Users\\Ruben\\Desktop\\androidAPP\\teen_power-android-app-0100bc6841bf\\app\\build\\intermediates\\instant-run-apk\\debug");
+        //File appDir = new File("C:\\Users\\Ruben\\Desktop\\teen_power-android-app-0100bc6841bf\\app\\build\\intermediates\\instant-run-apk\\debug");
+        File app = new File(appDir, "app-debug.apk");
+
+        // Capabilites
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("device","Android");
+
+        //mandatory capabilities
+        capabilities.setCapability("deviceName","appAndroid");
+        capabilities.setCapability("platformName","Android");
+
+        //other caps
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void testTestCasePerfil1() throws InterruptedException {
+        String app_package_name = "pt.ipleiria.teenpowerapp:id/";
+        By userId = By.id(app_package_name + "editTextName");
+        By password = By.id(app_package_name + "editTextUsername");
+        By login_Button = By.id(app_package_name + "buttonLogIn");
+
+        driver.findElement(userId).sendKeys("Testes01");
+        driver.findElement(password).sendKeys("password");
+        driver.findElement(login_Button).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("itemSave")).click();
+
+        Thread.sleep(2000);
+        String activity = ((AndroidDriver<MobileElement>) driver).currentActivity();
+        //System.out.println(activity);
+        Thread.sleep(2000);
+        Assert.assertEquals(".Game.HomeActivity",activity, "NÃO ESTOU NA ACTIVITY ESPERADA!!!");
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("menuItemProfile")).click();
+
+
+        Thread.sleep(2000);
+        String activity2 = ((AndroidDriver<MobileElement>) driver).currentActivity();
+        //System.out.println(activity);
+        Thread.sleep(2000);
+        Assert.assertEquals(".Profile.ProfileActivity",activity2, "NÃO ESTOU NA ACTIVITY ESPERADA!!!");
+    }
+}
